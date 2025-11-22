d458453af3e5b3d7269f5fecb0de2d8d53616cb3
Ahmed Mamdouh <amamdouh823@gmail.com>
2025-11-22 16:01:02 +0200

    implement IGenerisRepository

---
+++ infrastructure/Repos/GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Domain.Concrats;
+using Microsoft.EntityFrameworkCore;
+
+namespace infrastructure.Repos
+{
+    public class GenericRepository<T, TK> : IGenericRepository<T, TK> where T:BaseEntity<TK>
+    {
+        private readonly AppDbContext dbContext;
+
+        public GenericRepository(AppDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+           
+           return await dbContext.Set<T>().ToListAsync() ;   
+        }
+
+        public  async Task<T?> GetByIdAsync(TK id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+    }
+}
